8a13b45fe9e9c0c05db36ef83fceb6db0778cf9a
dhanupri <81618507+dhanupri@users.noreply.github.com>
2023-11-21 10:24:41 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -6,7 +6,17 @@ class LeapYear
 	Scanner sc=new Scanner(System.in);
 	int number=sc.nextInt();
 	if(number%4==0){
-		System.out.print(number+"is a leap year");
+		if(number%100==0){
+			if(number%400==0){
+				System.out.print(number+"is  a leap year");
+				
+			}
+			else{
+				System.out.print(number+"is not a leap year");
+			
+			}		
+		}
+		
 	}
 	else{
 		System.out.print(number+"is not a leap year");
